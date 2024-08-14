85b45df4af7158cb6027651559afff27f515af33
cornerbytes <cornerbytes@protonmail.com>
2024-08-14 23:29:05 +0700

    add chall final/misc/easy_casino

---
+++ final/crypto/easy_casino/server/chall.py
@@ -0,0 +1,28 @@
+FLAG = 'PCTF{mesin_casino_rusak_1337}'
+
+def convert_to_int(x, y):
+    try:
+        x = int(x, 16)
+        y = int(y, 16)
+        return x, y
+    except:
+        return False
+
+if __name__ == "__main__":
+    print('='*40)
+    print('Welcome to Easy_Casino!!!')
+    x = input('Enter first lucky number in hexadecimal : ')
+    y = input('Enter second lucky number in hexadecimal : ')
+
+    if convert_to_int(x, y) == False:
+        print('[NUMBER ERROR]')
+        exit()
+
+    if x != y:
+        x, y = convert_to_int(x, y)
+        if x == y :
+            print('[Congratulation you guess the same number!]')
+            print(FLAG)
+    else:
+        print('[DUPLICATE NUMBER]')
+    print('='*40)
